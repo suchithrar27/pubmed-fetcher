63ee6419231fe237d24e4e77c325da9a0e0d6a4d
suchithrar27 <138992392+suchithrar27@users.noreply.github.com>
2025-03-16 17:39:58 +0530

    Delete save_results.py

--- save_results.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def save_to_csv(data: list, filename: str):
-    """Save the list of paper details to a CSV file."""
-    df = pd.DataFrame(data)
-    df.to_csv(filename, index=False)
-    print(f"Results saved to {filename}")
